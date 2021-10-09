8197dda364aa14161c4de77d733cf80f0b03248f
Thomasolicious <thomas-delange@hotmail.com>
2021-10-09 20:18:01 +0200

    added config objects

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config(object):
+    """
+    Config object containing default configuriation
+    """
+    TESTING = False
+    SESSION_COOKIE_NAME = "session"
+    
+    
+class ProductionConfig(Config):
+    """
+    ProductionConfig class containing production configuriation, extending Config object
+    """
+    SECRET_KEY = os.environ.get("FLASK_PRODUCTION_SECRET_KEY")
+    
+    
+class DevelopmentConfig(Config):
+    """
+    DevelopmentConfig class containing development configuration, extending Config object
+    """
+    ENV = "development"
+    SECRET_KEY = os.environ.get("FLASK_DEVELOPMENT_SECRET_KEY")
+    SESSION_COOKIE_NAME = "dev_session"
+
+class TestingConfig(DevelopmentConfig):
+    """
+    TesttingConfig class containing development configuration, extending Config object
+    """
+    TESTING = True
+    SESSION_COOKIE_NAME = "test_session"
